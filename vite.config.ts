65d798368641a519c820500a15ec69e7709e3849
lio <licroning@163.com>
2022-08-29 00:39:52 +0800

    chore: mobile required deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
 import { URL, fileURLToPath } from 'node:url'
-
+import legacy from '@vitejs/plugin-legacy'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    // https://github.com/vitejs/vite/tree/main/packages/plugin-legacy
+    legacy({
+      targets: ['defaults', 'not IE 11'],
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
