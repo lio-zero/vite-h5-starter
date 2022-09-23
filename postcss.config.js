673c4967d9705e9800a5b74e2471c95f756daa5e
lio <licroning@163.com>
2022-09-24 03:33:39 +0800

    fix: handle vant viewport width problem

--- postcss.config.js
+++ postcss.config.js
@@ -5,15 +5,14 @@ module.exports = () => {
   return {
     plugins: [
       autoprefixer({ overrideBrowserslist: ['Android 4.1', 'iOS 7.1', 'Chrome > 31', 'ff > 31', 'ie >= 8'] }),
-      // https://github.com/vitejs/vite/issues/4653
+      /**
+       * vant 使用 375 尺寸，而开发时设计稿有可能是 750 或其他尺寸，这时 postcss-px-to-viewport 不好处理。
+       * 可参考：
+       * https://github.com/vitejs/vite/issues/4653
+       * https://blog.csdn.net/weixin_42998707/article/details/124150578
+       */
       px2viewport({
         viewportWidth: 375,
-        minPixelValue: 1,
-        unitPrecision: 5,
-        mediaQuery: true,
-      }),
-      px2viewport({
-        viewportWidth: 750,
         minPixelValue: 1,
         unitPrecision: 5,
         mediaQuery: true,
